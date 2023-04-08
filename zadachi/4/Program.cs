38866b62c8dd84c5cfb32c7b575dba459029d5a7
Ekaterina332 <rina73.73@mail.ru>
2023-04-08 08:05:34 +0300

    4

---
+++ zadachi/4/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+// int[, ] InputMatrix()
+// {
+//     Console.Write("Введите размер матрицы: ");
+//     int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+//     int[, ] matrix = new int[size[0], size[1]];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(1, 50);
+//             Console.Write($"{matrix[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+//     return matrix;
+// }
+
+
+void FillArraySpiral(int[,] array, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+Console.Clear();
+int dlina = 4;
+int[, ] matrix = new int[dlina, dlina];
+FillArraySpiral(matrix, dlina);
+Console.WriteLine();
+PrintArray(matrix);
